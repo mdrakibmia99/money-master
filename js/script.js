97ab3035eb22efbc46603cddd57a3bb8ea00b4e6
Rk Rakib hasan <rkrakibhasan680@gmail.com>
2022-02-17 17:48:45 +0600

    js complete

--- js/script.js
+++ js/script.js
@@ -22,25 +22,61 @@ document.getElementById("calculate-btn").addEventListener("click", function(e) {
     const currentbalance=totalIncome-totalCost;
      if((totalIncome<totalCost)){
         setValue("Expense over lap!!","calculate-error");
-        setValue("","total-expenses");
-        setValue("","toal-balance");
+        setValue("0","total-expenses");
+        setValue("0","toal-balance");
 
-    }else if( totalIncome>0 && foodCostValue>=0 && rentAmountValue>=0  && clothesAmountValue>=0){
+    }else if(!totalIncome>0){
+        setValue("0","total-expenses");
+        setValue("0","toal-balance");
+        setValue("Please Enter valid income value","calculate-error");
+
+    }
+    else if(  foodCostValue>=0 && rentAmountValue>=0  && clothesAmountValue>=0){
         setValue(totalCost,"total-expenses");
         setValue(currentbalance,"toal-balance");
         setValue("","calculate-error");
     }else{
-        setValue("","total-expenses");
-        setValue("","toal-balance");
-        setValue("Please Enter Number value","calculate-error");
+        setValue("0","total-expenses");
+        setValue("0","toal-balance");
+        setValue("Enter valid Expenses value","calculate-error");
     }
     
     
 
 })
 
-
+// function getInnerTexValue(innertextId){
+//   const textValue=document.getElementById(innertextId).innerText;
+//   const numberTextValue=getFloatVlue(textValue);
+//   return numberTextValue;
+// }
 document.getElementById("save-btn").addEventListener("click",function(){
-    const percentageValue=getInputValue("save-money-input");
-    console.log(percentageValue);
+    const Balance=document.getElementById("toal-balance").innerText;
+    const numericBalance=getFloatVlue(Balance);
+    const percentageInputValue=getInputValue("save-money-input");
+    const totalIncomeAmount=getInputValue("total-income");
+    const percentageAmount=totalIncomeAmount *(percentageInputValue/100);
+    const remainBalance=numericBalance-percentageAmount;
+  
+
+    if((percentageInputValue>0) && (percentageInputValue<=100)){
+        
+        if(numericBalance>=percentageAmount){
+            setValue(percentageAmount,"saving-money");
+            setValue(remainBalance,"remain-balance");
+            setValue("","save-btn-error");
+
+        }else{
+            setValue("0","saving-money");
+            setValue("0","remain-balance");
+            setValue("Expense over lap!!","save-btn-error");
+
+        }
+
+
+    }else{
+        setValue("0","saving-money");
+        setValue("0","remain-balance");
+        setValue(" Enter Number 1 to 100","save-btn-error");
+    }
 })
