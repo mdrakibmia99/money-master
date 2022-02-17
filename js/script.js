857540c7ea30f837389e971ad3d906cad5fb855a
Rk Rakib hasan <rkrakibhasan680@gmail.com>
2022-02-17 16:40:54 +0600

    calculation btn work done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-console.log("this is h1");
+function getFloatVlue(inputValue){
+    const inputNumberValue=parseFloat(inputValue);
+    return inputNumberValue;
+}
+
+function getInputValue(inputId){
+
+    const inputValueSelect=document.getElementById(inputId).value;
+    const Numbervalue=getFloatVlue(inputValueSelect);
+    return Numbervalue;
+}
+function setValue(setValue,setId){
+    document.getElementById(setId).innerText=setValue;
+}
+
+document.getElementById("calculate-btn").addEventListener("click", function(e) {
+    const totalIncome=getInputValue("total-income");
+    const foodCostValue=getInputValue("food-cost");
+    const rentAmountValue=getInputValue("rent-amount");
+    const clothesAmountValue=getInputValue("clothes-cost");
+    const totalCost=foodCostValue+rentAmountValue+clothesAmountValue;
+    const currentbalance=totalIncome-totalCost;
+     if((totalIncome<totalCost)){
+        setValue("Expense over lap!!","calculate-error");
+        setValue("","total-expenses");
+        setValue("","toal-balance");
+
+    }else if( totalIncome>0 && foodCostValue>=0 && rentAmountValue>=0  && clothesAmountValue>=0){
+        setValue(totalCost,"total-expenses");
+        setValue(currentbalance,"toal-balance");
+        setValue("","calculate-error");
+    }else{
+        setValue("","total-expenses");
+        setValue("","toal-balance");
+        setValue("Please Enter Number value","calculate-error");
+    }
+    
+    
+
+})
+
+
+document.getElementById("save-btn").addEventListener("click",function(){
+    const percentageValue=getInputValue("save-money-input");
+    console.log(percentageValue);
+})
